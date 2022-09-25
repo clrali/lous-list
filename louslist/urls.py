48d1d72b56ae395f73a3f434a31e94e3b1329afc
clara li <clara4695@gmail.com>
2022-09-25 15:40:06 -0400

    added Procfile

--- louslist/urls.py
+++ louslist/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# SHERRIFF: added the '' path to handle the root url
 urlpatterns = [
+    path('', include('myapp.urls')),
     path('louslist/', include('louslistapp.urls')),
     path('admin/', admin.site.urls),
 ]
