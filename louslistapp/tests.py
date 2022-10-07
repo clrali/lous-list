0f79a6941673b24d415808247a1e95be538fa36a
clara li <clara4695@gmail.com>
2022-10-07 10:54:41 -0400

    working test case

--- louslistapp/tests.py
+++ louslistapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+import unittest
 
 # Create your tests here.
+class BasicTest(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual("hello".upper(), "HELLO")
+    
+if __name__ == "__main__":
+    unittest.main()
