790252854450acbeae347c275c4e76b303ba8907
clara li <clara4695@gmail.com>
2022-10-07 10:10:37 -0400

    working google login

--- louslist/settings.py
+++ louslist/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 # needed for Google authentication
 SITE_ID = 2
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/home"
 
 # Additional configuration settings
 SOCIALACCOUNT_QUERY_EMAIL = True
