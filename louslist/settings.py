fef304a4a583a34392c55c7f9626b14d7598fd7d
clara li <clara4695@gmail.com>
2022-10-07 10:19:13 -0400

    final changes

--- louslist/settings.py
+++ louslist/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'louslistapp.apps.LouslistappConfig',
 
     'allauth',
     'allauth.account',
