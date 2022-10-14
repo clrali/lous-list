572b1da7fd1297946cfe4f33722864dc33b99361
msboddu <43827712+msboddu@users.noreply.github.com>
2022-10-14 15:05:58 -0400

    added dropdown features

--- louslistapp/views.py
+++ louslistapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
-# Create your views here.
+# Create your views here.git
 def home(request):
     return render(request, 'home.html')
+
+def dept_dropdown(request):
+    response = requests.get('http://luthers-list.herokuapp.com/api/deptlist')
+    depts = response.json()
+
+    return render(request, 'dropdown.html', {'depts': depts})
 
 def login(request):
     return render(request, 'login.html')
