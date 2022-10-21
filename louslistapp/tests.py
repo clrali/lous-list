8854155dcd217e91b2e25df6981142462882a1d8
clara li <clara4695@gmail.com>
2022-10-21 01:11:43 -0400

    testing api calls

--- louslistapp/tests.py
+++ louslistapp/tests.py
@@ -1,10 +1,38 @@
 from django.test import TestCase
 import unittest
+from .models import Instructor
+import requests
 
 # Create your tests here.
-class BasicTest(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("hello".upper(), "HELLO")
-    
-if __name__ == "__main__":
+
+
+class InstructorModelTest(unittest.TestCase):
+    def test_api_returns_correct_professor(self):
+        url = 'http://luthers-list.herokuapp.com/api/dept/CS'
+        response = requests.get(url)
+        courses = response.json()[0]
+
+        test_instructor = Instructor.objects.create(
+            prof_name=courses['instructor']['name'], prof_email=courses['instructor']['email'])
+        expected_instructor = Instructor.objects.create(
+            prof_name="Derrick Stone", prof_email="djs6d@virginia.edu")
+
+        self.assertEqual(test_instructor.prof_name,
+                         expected_instructor.prof_name, "The instructor is not as expected")
+
+    def test_api_returns_incorrect_professor(self):
+        url = 'http://luthers-list.herokuapp.com/api/dept/CS'
+        response = requests.get(url)
+        courses = response.json()[1]
+
+        test_instructor = Instructor.objects.create(
+            prof_name=courses['instructor']['name'], prof_email=courses['instructor']['email'])
+        expected_instructor = Instructor.objects.create(
+            prof_name="Derrick Stone", prof_email="djs6d@virginia.edu")
+
+        self.assertNotEqual(test_instructor.prof_name,
+                            expected_instructor.prof_name, "The instructor is the same")
+
+
+if __name__ == '__main__':
     unittest.main()
--- louslistapp/views.py
+++ louslistapp/views.py
@@ -2,57 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
+
 def home(request):
     return render(request, 'home.html')
-
-def dept_dropdown(request):
-    # For department dropdown
-    url = 'http://luthers-list.herokuapp.com/api/deptlist'
-    response = requests.get(url)
-    departments = response.json()
-
-    # For courses
-    query = request.GET
-    courses, course_dict = None, None
-    if query is not None and 'q' in query:
-        dept_name = query.get('q')
-        url = 'http://luthers-list.herokuapp.com/api/dept/' + dept_name
-        response = requests.get(url)
-        courses = response.json()
-
-        # This will make sure that all sections of a course are grouped together
-        course_dict = {}
-        for course in courses:
-            course_name = (course['subject'], course['catalog_number'], course['description'],
-                           course['instructor']['name'], course['instructor']['email'])
-            if course_name in course_dict:
-                course_dict[course_name].append(course)
-            else:
-                course_dict[course_name] = [course]
-
-    return render(request, 'dropdown.html', {'departments': departments, 'courses': course_dict})
 
 
 def login(request):
     return render(request, 'login.html')
 
+
 def viewdepts(request):
-    response = requests.get('http://luthers-list.herokuapp.com/api/deptlist/?format=json').json()
+    response = requests.get(
+        'http://luthers-list.herokuapp.com/api/deptlist/?format=json').json()
     return render(request, 'dropdown.html', {'response': response})
 
+
 def viewcourses(request):
-    response = requests.get('http://luthers-list.herokuapp.com/api/dept/ENGR?format=json').json()
+    response = requests.get(
+        'http://luthers-list.herokuapp.com/api/dept/ENGR?format=json').json()
     return render(request, 'courses.html', {'response': response})
 
-#def viewcourseSections(request):
-    #return render(request, 'courseSections.html', {'response': response})
+# def viewcourseSections(request):
+    # return render(request, 'courseSections.html', {'response': response})
+
 
 def dept_dropdown(request):
     # For department dropdown
     url = 'http://luthers-list.herokuapp.com/api/deptlist'
     response = requests.get(url)
     departments = response.json()
-    #print(response)
+    # print(response)
 
     # For courses
     query = request.GET
@@ -62,44 +41,47 @@ def dept_dropdown(request):
         print(dept_name)
         course_num = None
         prof_name = None
-        if('n' in query):
+        if ('n' in query):
             course_num = query.get('n')
         if ('p' in query):
             prof_name = query.get('p')
-        #print(course_num)
-        #print(prof_name)
+        # print(course_num)
+        # print(prof_name)
         url = 'http://luthers-list.herokuapp.com/api/dept/' + dept_name
         response = requests.get(url)
         courses = response.json()
 
-
         # This will make sure that all sections of a course are grouped together
         course_dict = {}
         for course in courses:
-            #print(course['instructor']["name"])
-            if(course_num == "" and prof_name==""):
-                course_name = (course['subject'], course['catalog_number'], course['description'])
+            # print(course['instructor']["name"])
+            if (course_num == "" and prof_name == ""):
+                course_name = (
+                    course['subject'], course['catalog_number'], course['description'])
                 if course_name in course_dict:
                     course_dict[course_name].append(course)
                 else:
                     course_dict[course_name] = [course]
 
             elif (course_num == course['catalog_number'] and prof_name == ""):
-                course_name = (course['subject'], course['catalog_number'], course['description'])
+                course_name = (
+                    course['subject'], course['catalog_number'], course['description'])
                 if course_name in course_dict:
                     course_dict[course_name].append(course)
                 else:
                     course_dict[course_name] = [course]
 
             elif (course_num == "" and prof_name.lower() in course['instructor']["name"].lower()):
-                course_name = (course['subject'], course['catalog_number'], course['description'])
+                course_name = (
+                    course['subject'], course['catalog_number'], course['description'])
                 if course_name in course_dict:
                     course_dict[course_name].append(course)
                 else:
                     course_dict[course_name] = [course]
 
             elif (course_num == course['catalog_number'] and prof_name.lower() in course['instructor']["name"].lower()):
-                course_name = (course['subject'], course['catalog_number'], course['description'])
+                course_name = (
+                    course['subject'], course['catalog_number'], course['description'])
                 if course_name in course_dict:
                     course_dict[course_name].append(course)
                 else:
